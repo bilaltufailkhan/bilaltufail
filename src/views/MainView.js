43e92e4fc9b93b0220492a25edc710e8f0643c3f
Bilal Tufail <thesabtech@gmail.com>
2023-11-07 03:51:56 -0800

    footer import again fixed

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import About from '../components/About';
 import Experience from '../components/Experience';
-import Footer from '../components/footer';
+// import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
 import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 import Slider from '../components/Slider';
+import { Footer } from '../components';
 
 const AdminView = props => {
   return (
