88e0ccbd86ae09c22e723883180ef6991565fb66
Bilal Tufail <thesabtech@gmail.com>
2023-11-07 03:33:28 -0800

    somethigns fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+require('events').EventEmitter.defaultMaxListeners = 1500;
+
 // Import CSS here+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./assets/css/style.css"
+import './assets/css/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
